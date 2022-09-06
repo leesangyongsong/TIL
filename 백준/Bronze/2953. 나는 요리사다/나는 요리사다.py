3dd59b8f91ced80912941bf7d32d85424850207b
leesangyong94 <108654815+leesangyongsong@users.noreply.github.com>
2022-09-06 22:09:27 +0900

    [Bronze III] Title: 나는 요리사다, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -1,15 +1,4 @@
-a = sum(list(map(int, input().split())))
-b = sum(list(map(int, input().split())))
-c = sum(list(map(int, input().split())))
-d = sum(list(map(int, input().split())))
-e = sum(list(map(int, input().split())))
-if max(a, b, c, d, e) == a:
-  print(1, a)
-elif max(a, b, c, d, e) == b:
-  print(2, b)
-elif max(a, b, c, d, e) == c:
-  print(3, c)
-elif max(a, b, c, d, e) == d:
-  print(4, d)
-else:
-  print(5, e)
+score = []
+for i in range(5):
+  score.append(sum(map(int, input().split())))
+print(score.index(max(score))+1,max(score))
